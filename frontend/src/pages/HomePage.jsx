9f21149e2a840273bf71c9a3b16a52ef4390f53b
Fuky9 <lukasfukan6@gmail.com>
2026-04-18 22:13:34 +0200

    feat: add note deletion and empty state UI

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 import api from "../lib/axios";
 import toast from "react-hot-toast";
+
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(null);
@@ -39,12 +41,14 @@ const HomePage = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
-        {notes.length > 0 && !isRateLimited && (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
-            {notes.map(note => (
-              <NoteCard key={note._id} note={note} />
-            ))}
-          </div>
+      {!loading && notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
+      {notes.length > 0 && !isRateLimited && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+          {notes.map(note => (
+            <NoteCard key={note._id} note={note} setNotes={setNotes} />
+          ))}
+        </div>
         )}
       </div>
     </div>
